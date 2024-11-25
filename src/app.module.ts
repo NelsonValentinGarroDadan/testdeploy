4a406e323258bac4f304472151974b791dd5a8e1
Velentin Garro <nelsonvgarrodadan@gmail.com>
2024-11-25 11:01:14 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import ormConfig from './config/ormConfig';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { usersmodule } from '../src/modules/users/users.module';
+import { usersmodule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { ActivityModule } from './modules/activities/Activity.module';
 import { CategoryModule } from './modules/categories/category.module';
