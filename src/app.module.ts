d4ed1a346969b7ec5a8339f148980f1dd1916697
Velentin Garro <nelsonvgarrodadan@gmail.com>
2024-11-26 11:42:54 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import ormConfig from '../src/orm-config';
+import ormConfig from './src/orm-config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersModule } from '../src/modules/users/users.module';
-import { AuthModule } from '../src/modules/auth/auth.module';
-import { ActivityModule } from '../src/modules/activities/activity.module';
-import { CategoryModule } from '../src/modules/categories/category.module';
-import { StripeModule } from '../src/modules/stripe/stripe.module';
+import { UsersModule } from './src/modules/users/users.module';
+import { AuthModule } from './src/modules/auth/auth.module';
+import { ActivityModule } from './src/modules/activities/activity.module';
+import { CategoryModule } from './src/modules/categories/category.module';
+import { StripeModule } from './src/modules/stripe/stripe.module';
 
 @Module({
   imports: [
