aa416d1dcfec09fa56101d03c2f250e5a44b936c
Velentin Garro <nelsonvgarrodadan@gmail.com>
2024-11-26 10:06:02 -0300

    rutas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import ormConfig from '../config/ormConfig';
+import ormConfig from '../config/orm-config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from '../modules/users/users.module';
 import { AuthModule } from '../modules/auth/auth.module';
-import { ActivityModule } from '../modules/activities/Activity.module';
+import { ActivityModule } from '../modules/activities/activity.module';
 import { CategoryModule } from '../modules/categories/category.module';
 import { StripeModule } from '../modules/stripe/stripe.module';
 
